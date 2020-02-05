06df61acebd74dd3b57dc9de9d32516ddfe1c0d5
thetarkus <mike@mikemob.com>
2020-02-05 14:53:40 -0500

    Change borderpx to 2

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 #include "push.c"
 
 /* appearance */
-static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int gappx     = 0;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
